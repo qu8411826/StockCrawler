6cf7bf318d66f5711a10d18dfec1aa30298b71e2
Leon Huang <leon@amy-leon.idv.tw>
2018-09-23 11:14:57 +0800

    Version 5.0

--- Source/StockCrawler.py
+++ Source/StockCrawler.py
@@ -8,38 +8,13 @@ import matplotlib.pyplot as plt
 import time
 from pathlib import Path
 import os
-  
-combined_quarterly_tables = [
-        'SII綜合損益彙總表',
-        'OTC綜合損益彙總表',
-        'SII資產負債彙總表',
-        'OTC資產負債彙總表',
-        'SII營益分析彙總表',
-        'OTC營益分析彙總表']
 
-combined_monthly_tables = [
-        'SII每月營業收入彙總表',
-        'OTC每月營業收入彙總表']
+from quarterly_statement import quarterly_statement
+from combined_monthly_revenue_report import combined_monthly_revenue_report
+from individual_quarterly_report import individual_quarterly_report
+from individual_monthly_trading_record import individual_monthly_trading_record
 
 # main configurations
-quarter_range = {
-#        2013: range(1,5),
-#        2014: range(1,5),
-#        2015: range(1,5),
-#        2016: range(1,5),
-        2017: range(1,4),
-        2018: range(1,1) # as of 2018/03/11 no data for Q1 yet
-}
-month_range = {
-#        2013: range(1,13),
-#        2014: range(1,13),
-#        2015: range(1,13),
-#        2016: range(1,13),
-        2017: range(1,13),
-        2018: range(1,1) # as of 2018/03/11 no data for Q1 yet
-}
-update_latest_monthly = False # flag to update un-finished or latest monthly reports
-
 IPO_Date = {
     1817:(2013, 10, 24),
     3558:(2013, 12, 30),
@@ -51,11 +26,6 @@ IPO_Date = {
     4126:(2013,  1,  1), # this company has no data for 2012, but I don't know why???
     4205:(2013,  1,  1) # this company has no data for 2012, but I don't know why???
 }
-
-from quarterly_statement import quarterly_statement
-from combined_monthly_revenue_report import combined_monthly_revenue_report
-from individual_quarterly_report import individual_quarterly_report
-from individual_monthly_trading_record import individual_monthly_trading_record
 
 # read target stock list
 ref_xls_file_name = "HG交易系統.xlsx"
@@ -69,64 +39,27 @@ ref_xls_reader.close()
 ref_df = ref_df.set_index("代號")
 target_stocks = list(ref_df.index)
 
-"""
-# get 月成交資訊 trading record for interested stocks
-raw_individual_annual_trading_table = {}
-for year in quarter_range.keys() :
-    path_name = './'+str(year)+'/'
-    directory = os.path.dirname(path_name)
-    try:
-        os.stat(directory)
-    except:
-        os.mkdir(directory)
-    month_string = "trading_%4d0101" %year
-    for co_id in target_stocks:
-        if (co_id in IPO_Date.keys()): # this stock is still young
-            if (year < IPO_Date[co_id][0]):
-                continue
-        co_name = ref_df.loc[co_id, '公司']
-        sheet_name = "%s_%d_%s" %(month_string, co_id, co_name)
-        xls_file_name = path_name + month_string + '_' + str(co_id) + '.xlsx'
-        xls_file_now = Path(xls_file_name)
-        if (xls_file_now.is_file()) :
-            # xls file exists, read tables from xls
-            xls_reader = pd.ExcelFile(xls_file_name)
-            try:
-                df_now = xls_reader.parse(sheet_name)
-            except:
-                df_now = None
-            # end of co_id loop
-            xls_reader.close()
-            if (df_now is not None):
-                if ('月份' in df_now.columns):
-                    df_now = df_now.set_index('月份')
-                else:
-                    assert('月' in df_now.columns)
-                    if (df_now.iloc[0][0] == '月'):
-                        df_now = df_now.drop(index=0)
-                        xls_writer = pd.ExcelWriter(xls_file_name, engine='xlsxwriter')
-                        df_now.to_excel(xls_writer, sheet_name=sheet_name)
-                        xls_writer.save()
-                    df_now = df_now.set_index('月')
-                if (df_now.index[-1] == 12 or df_now.index[-1] == str(12) or \
-                    (year == year_range[-1] and update_latest_monthly == False)) :
-                    print("Got %s from xls" %sheet_name)
-                    # save df_now to the array
-                    raw_individual_annual_trading_table[sheet_name] = df_now
-                    continue # next co_id
-        # read tables from web and save to xls
-        df_now = individual_monthly_trading_record(co_id, co_name, year, ref_df)
-        raw_individual_annual_trading_table[sheet_name] = df_now
-        if (df_now is not None):
-            print("Writing %s to %s" %(sheet_name, xls_file_name))
-            xls_writer = pd.ExcelWriter(xls_file_name, engine='xlsxwriter')
-            df_now.to_excel(xls_writer, sheet_name=sheet_name)
-            xls_writer.save()
-        else:
-            print("Failed getting %s for %s" %(sheet_name, xls_file_name))
-    # for co_id
-# end of year loop
+quarter_range = {
+#        2013: range(1,5),
+#        2014: range(1,5),
+#        2015: range(1,5),
+        2016: range(1,5),
+#        2017: range(1,4),
+#        2018: range(1,1) # as of 2018/03/11 no data for Q1 yet
+}
 
+"""
+combined_quarterly_tables = [
+        'SII綜合損益彙總表',
+        'OTC綜合損益彙總表',
+        'SII資產負債彙總表',
+        'OTC資產負債彙總表',
+        'SII營益分析彙總表',
+        'OTC營益分析彙總表']
+
+combined_monthly_tables = [
+        'SII每月營業收入彙總表',
+        'OTC每月營業收入彙總表']
 
 raw_quarterly_table = {}
 raw_monthly_table = {}
@@ -283,6 +216,73 @@ for year in quarter_range.keys() :
     # done looping quarter
 # done looping year
 
+"""
+# get 月成交資訊 trading record for interested stocks
+month_range = {
+#        2013: range(1,13),
+#        2014: range(1,13),
+#        2015: range(1,13),
+#        2016: range(1,13),
+        2017: range(1,13),
+        2018: range(1,1) # as of 2018/03/11 no data for Q1 yet
+}
+update_latest_monthly = False # flag to update un-finished or latest monthly reports
+raw_individual_monthly_trading_table = {}
+for year in month_range.keys() :
+    path_name = './'+str(year)+'/'
+    directory = os.path.dirname(path_name)
+    try:
+        os.stat(directory)
+    except:
+        os.mkdir(directory)
+    month_string = "trading_%4d0101" %year
+    for co_id in target_stocks:
+        if (co_id in IPO_Date.keys()): # this stock is still young
+            if (year < IPO_Date[co_id][0]):
+                continue
+        co_name = ref_df.loc[co_id, '公司']
+        sheet_name = "%s_%d_%s" %(month_string, co_id, co_name)
+        xls_file_name = path_name + month_string + '_' + str(co_id) + '.xlsx'
+        xls_file_now = Path(xls_file_name)
+        if (xls_file_now.is_file()) :
+            # xls file exists, read tables from xls
+            xls_reader = pd.ExcelFile(xls_file_name)
+            try:
+                df_now = xls_reader.parse(sheet_name)
+            except:
+                df_now = None
+            # end of co_id loop
+            xls_reader.close()
+            if (df_now is not None):
+                if ('月份' in df_now.columns):
+                    df_now = df_now.set_index('月份')
+                else:
+                    assert('月' in df_now.columns)
+                    if (df_now.iloc[0][0] == '月'):
+                        df_now = df_now.drop(index=0)
+                        xls_writer = pd.ExcelWriter(xls_file_name, engine='xlsxwriter')
+                        df_now.to_excel(xls_writer, sheet_name=sheet_name)
+                        xls_writer.save()
+                    df_now = df_now.set_index('月')
+                if (df_now.index[-1] == 12 or df_now.index[-1] == str(12) or \
+                    (year == year_range[-1] and update_latest_monthly == False)) :
+                    print("Got %s from xls" %sheet_name)
+                    # save df_now to the array
+                    raw_individual_monthly_trading_table[sheet_name] = df_now
+                    continue # next co_id
+        # no previous data, read tables from web and save to xls
+        df_now = individual_monthly_trading_record(co_id, co_name, year, ref_df)
+        raw_individual_monthly_trading_table[sheet_name] = df_now
+        if (df_now is not None):
+            print("Writing %s to %s" %(sheet_name, xls_file_name))
+            xls_writer = pd.ExcelWriter(xls_file_name, engine='xlsxwriter')
+            df_now.to_excel(xls_writer, sheet_name=sheet_name)
+            xls_writer.save()
+        else:
+            print("Failed getting %s for %s" %(sheet_name, xls_file_name))
+    # for co_id
+# end of year loop
+"""
 
 print ("Finished DataFrame preparation! Next step is to prepare data into Excel Stock model!")
 
